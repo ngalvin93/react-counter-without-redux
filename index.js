c556445c33f611d940190f0ce604d85b4caf4be9
Alvin Ng <ngalvin93@gmail.com>
2019-11-18 23:05:17 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Counter extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    countUp = () => {
+        const stateCopy = {...this.state}
+        console.log(stateCopy)
+        const newCount = stateCopy.count + 1
+        console.log(newCount)
+        this.setState({
+            count: newCount
+        })
+    }
+
+    countDown = () => {
+        const stateCopy = {...this.state}
+        console.log(stateCopy)
+        const newCount = stateCopy.count - 1
+        console.log(newCount)
+        this.setState({
+            count: newCount
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.state.count }</h1>
+                <button onClick={ this.countUp }>+</button>
+                <button onClick={ this.countDown }>-</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
